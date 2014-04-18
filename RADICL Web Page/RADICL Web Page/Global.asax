ff01df2d637bc77169d47b3a50c7094e374b9457
NateKrussel <nate.krussel@gmail.com>
2014-04-17 22:06:57 -0700

    Initial MVC Framework Set up

---
+++ RADICL Web Page/RADICL Web Page/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RADICL_Web_Page.MvcApplication" Language="C#" %>
---
+++ RADICL Web Page/RADICL Web Page/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RADICL_Web_Page.Startup))]
+namespace RADICL_Web_Page
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
